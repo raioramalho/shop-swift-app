c279cf1d02e9e16d5cff1619055ed1020a49c785
Alan Ramalho <ramalho.sit@gmail.com>
2024-10-25 21:17:05 -0300

    Initial Commit

---
+++ shop/shopApp.swift
@@ -0,0 +1,17 @@
+//
+//  shopApp.swift
+//  shop
+//
+//  Created by Alan Ramalho on 25/10/24.
+//
+
+import SwiftUI
+
+@main
+struct shopApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
